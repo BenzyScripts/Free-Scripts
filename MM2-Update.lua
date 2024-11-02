59389a96d0e0c302ba34f6fad0bc9caf8cf3e720
@WesleyzinXk <153573578+Wesleyzinx@users.noreply.github.com>
2024-11-02 00:08:33 -0300

    Update MM2-Update.lua

--- MM2-Update.lua
+++ MM2-Update.lua
@@ -1,1 +1,1 @@
-https://github.com/Wesleyzinx/Scripts-Roblox-2024-/raw/refs/heads/main/MM2-november
+loadstring(game:HttpGet("https://github.com/Wesleyzinx/Scripts-Roblox-2024-/raw/refs/heads/main/MM2-november"))()
