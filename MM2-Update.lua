d1359ade7e0347c9c8df032976d4ca4896fed959
@BenzyScripts <153573578+BenzyScripts@users.noreply.github.com>
2024-11-02 23:47:00 -0300

    Update MM2-Update.lua

--- MM2-Update.lua
+++ MM2-Update.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/Wesleyzinx/Scripts-Roblox-2024-/raw/refs/heads/main/MM2-november"))()
+loadstring(game:HttpGet("https://github.com/BenzyScripts/Scripts-Roblox-2024-/raw/refs/heads/main/MM2-november"))()
