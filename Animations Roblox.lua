58726ad54460caf54352e5349ee2ec2ece5870e6
@BenzyScripts <153573578+BenzyScripts@users.noreply.github.com>
2024-11-02 23:48:21 -0300

    Update Animations Roblox.lua

--- Animations Roblox.lua
+++ Animations Roblox.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/Wesleyzinx/Scripts-Roblox-2024-/raw/refs/heads/main/Anim.lua"))()
+loadstring(game:HttpGet("https://github.com/BenzyScripts/Scripts-Roblox-2024-/raw/refs/heads/main/Anim.lua"))()
